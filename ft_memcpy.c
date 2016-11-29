0f4262c3b3ae5381b3b391698a5db42e9dec3049
Maxwell BURSON <mburson@e1z2r3p26.42.us.org>
2016-11-28 19:19:29 -0800

    forogt to add the files post move

---
+++ ft_memcpy.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mburson <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/28 16:56:22 by mburson           #+#    #+#             */
+/*   Updated: 2016/11/28 16:56:24 by mburson          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+//comment
+#include <string.h>
+
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+{
+	size_t i;
+
+	i = 0;
+	while (i < n)
+	{
+		((char*)dst)[i] = ((char*)src)[i];
+		i++;
+	}
+	return (dst);
+}
