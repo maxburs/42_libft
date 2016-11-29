40dc878a2688ede8b080564e7858b3da3097a9ce
Maxwell BURSON <mburson@e1z2r3p26.42.us.org>
2016-11-28 20:04:19 -0800

    forgot to remove temp comment

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -9,7 +9,7 @@
 /*   Updated: 2016/11/28 16:56:24 by mburson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-//comment
+
 #include <string.h>
 
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
