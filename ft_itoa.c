a33f4828113033a2947053914ac626b69cd7b3eb
Maxwell BURSON <mburson@e1z2r3p26.42.us.org>
2016-12-03 21:56:32 -0800

    fixed ft_itoa not handling 0 correctly

--- ft_itoa.c
+++ ft_itoa.c
@@ -20,10 +20,8 @@ char	*ft_itoa(int n)
 
 	if (n == -2147483648)
 		return (ft_strdup("-2147483648"));
-	
 	if (n == 0)
 		return (ft_strdup("0"));
-	
 	i = ft_numlen((long)n, 10);
 	if (!(word = (char*)malloc(sizeof(char) * (i + 1))))
 		return (NULL);
