392262e4c3225eee8ed2c71c4d2d7c4169452868
Maxwell BURSON <mburson@e1z2r3p26.42.us.org>
2016-12-03 22:12:45 -0800

    fixed not allocating space for the null terminator in ft_strmap and ft_strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -19,7 +19,7 @@ char	*ft_strmapi(char *s, char (*f)(unsigned int, char))
 	unsigned int	i;
 	char			*new;
 
-	if (!(new = (char*)malloc(sizeof(char) * ft_strlen(s))))
+	if (!(new = (char*)malloc(sizeof(char) * (ft_strlen(s) + 1))))
 		return (NULL);
 	i = 0;
 	while (s[i])
