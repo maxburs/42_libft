fab3c57b8bc4f4c02256dd697d8546d88d7dcb0e
Maxwell BURSON <mburson@e1z2r8p26.42.us.org>
2017-01-20 17:05:50 -0800

    bugfixed ft_utf_32_8

--- convert/ft_utf_32_8.c
+++ convert/ft_utf_32_8.c
@@ -29,25 +29,25 @@ static unsigned char     format_first(unsigned char c, int length)
 
 static char                get_length(wint_t c)
 {
-    if ((0xFFFFFFFF << 7) & c)
+    if (!((0xFFFFFFFF << 7) & c))
         return (1);
-    if ((0xFFFFFFFF << 11) & c)
+    if (!((0xFFFFFFFF << 11) & c))
         return (2);
-    if ((0xFFFFFFFF << 16) & c)
+    if (!((0xFFFFFFFF << 16) & c))
         return (3);
-    if ((0xFFFFFFFF << 21) & c)
+    if (!((0xFFFFFFFF << 21) & c))
         return (4);
     return (0);
 }
 
-unsigned char    *ft_utf_32_8(wint_t c)
+unsigned char				*ft_utf_32_8(wint_t c)
 {
     unsigned char   *res;
     int             len;
+    int             i;
 
     if (!(len = get_length(c)))
         return (NULL);
-    printf("length: %d\n", len);
     if (!(res = (unsigned char*)malloc(sizeof(char) * (len + 1))))
         return (NULL);
     res[len] = '\0';
@@ -56,14 +56,18 @@ unsigned char    *ft_utf_32_8(wint_t c)
         res[0] = (unsigned char)c;
         return (res);
     }
-    while (len)
+    i = len;
+    while (i)
     {
-        len--;
-        res[len] = (unsigned char)c;
-        if (len)
-            res[len] = format_trailing(res[len]);
+        i--;
+        res[i] = (unsigned char)c;
+        if (i)
+        {
+            res[i] = format_trailing(res[i]);
+            c = c >> 6;
+        }
         else
-            res[len] = format_first(res[len], len);
+            res[i] = format_first(res[i], len);
     }
     return (res);
 }
